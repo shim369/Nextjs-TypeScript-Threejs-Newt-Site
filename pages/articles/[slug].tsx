d04178f4059f47164b65bd895ab01d1cfe68afb8
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-04-20 20:36:36 +0900

    change head

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import HeaderInner from '../../components/HeaderInner'
 import Contact from '../../components/Contact'
 import Footer from '../../components/Footer'
